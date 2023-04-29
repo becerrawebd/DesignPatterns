a1087e12264ff99f90d58d13ebc924da8e5366f4
Diego Becerra <becerradiego94@gmail.com>
2023-04-29 02:00:19 -0300

    fix log observer

--- observer/src/main/java/org/designpatterns/Main.java
+++ observer/src/main/java/org/designpatterns/Main.java
@@ -4,17 +4,18 @@ import org.designpatterns.subscribers.FileLoggingListener;
 import org.designpatterns.subscribers.StdOutLoggingListener;
 
 import java.io.IOException;
+import java.nio.file.Path;
 
 public class Main {
     public static void main(String[] args) {
         Editor editor = new Editor();
         StdOutLoggingListener stdOutLoggingListener = new StdOutLoggingListener();
-        FileLoggingListener fileLoggingListener = new FileLoggingListener("logs.txt");
+        FileLoggingListener fileLoggingListener = new FileLoggingListener("observer/logs.txt");
         editor.getEventManager().subscribe(FileEventType.OPEN, stdOutLoggingListener);
         editor.getEventManager().subscribe(FileEventType.WRITE, stdOutLoggingListener);
         editor.getEventManager().subscribe(FileEventType.OPEN, fileLoggingListener);
         editor.getEventManager().subscribe(FileEventType.DELETE, fileLoggingListener);
-        editor.openFile("file.txt");
+        editor.openFile("observer/file.txt");
         try {
             editor.writeFile("Hello World");
             editor.deleteFile();
